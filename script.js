f15ae34f04772d88596fadc0bf177ee6af74b3fe
Huelsgoat <huntsperger@gmail.com>
2020-03-11 00:38:33 -0700

    it's working, needs a little polishing up

--- script.js
+++ script.js
@@ -12,3 +12,70 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+function generatePassword() {
+
+  var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var specials = ["!", "@", "#", "$", "%", "&", "*"];
+  var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  var userChoice = [];
+  var password = [];
+  var howLong = parseInt(prompt("Please choose a length between 8 and 128 characters for your password"));
+  var hasNums = confirm("Would you like to include numbers?");
+  var hasLower = confirm("Would you like your password to include lowercase letters?");
+  var hasUpper = confirm("Would you like your password to include uppercase letters?");
+  var hasChars = confirm("Would you like your password to include special characters?");
+
+
+  if (howLong >= 8 && howLong <= 128 && typeof howLong === "number") {
+    if (hasNums || hasLower || hasUpper || hasChars) {
+      makeArr();
+    } else {
+      alert("Password must include characters!");
+    }
+  } else {
+    alert("must be number between 8 and 128");
+  }
+
+
+  // I tried a few different ways to get the userChoice array. Doing a slice with no argument each time through the iteration worked okay, but what I did below makes the most sense to me... 
+
+  function makeArr() {
+
+    if (hasNums) {
+      for (let i = 0; i < numbers.length; i++) {
+        userChoice[userChoice.length] = numbers[i];
+      }
+    }
+
+    if (hasLower) {
+      for (let j = 0; j < letters.length; j++) {
+        userChoice[userChoice.length] = letters[j];
+      }
+    }
+
+    if (hasUpper) {
+      for (let k = 0; k < letters.length; k++) {
+        userChoice[userChoice.length] = letters[k].toUpperCase();
+      }
+    }
+
+    if (hasChars) {
+      for (let l = 0; l < specials.length; l++) {
+        userChoice[userChoice.length] = specials[l];
+      }
+    }
+    return userChoice
+  }
+
+
+  for (let index = 0; index < howLong; index++) {
+    password[index] = userChoice[Math.floor(Math.random() * userChoice.length)];
+
+  }
+  password = password.join("");
+
+  return password;
+
+}
--- workingscript.js
+++ workingscript.js
@@ -1,9 +1,11 @@
+
+
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specials = ["!", "@", "#", "$", "%", "&", "*", "(", ")"];
+var specials = ["!", "@", "#", "$", "%", "&", "*"];
 var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var userChoice = [];
-
-var howLong = prompt("Please choose a length between 8 and 128 characters for your password");
+var password = [];
+var howLong = parseInt(prompt("Please choose a length between 8 and 128 characters for your password"));
 var hasNums = confirm("Would you like to include numbers?");
 var hasLower = confirm("Would you like your password to include lowercase letters?");
 var hasUpper = confirm("Would you like your password to include uppercase letters?");
@@ -19,7 +21,7 @@ if (howLong >= 8 || howLong <= 128) {
     }
 } else {
     alert("must be number between 8 and 128");
-} 
+}
 
 // else if (!hasNums && !hasLower && !hasUpper && !hasChars) {
 //     alert("Password must include at least one character type.");
@@ -31,34 +33,45 @@ if (howLong >= 8 || howLong <= 128) {
 
 function makeArr() {
 
-if (hasNums) {
-    for (let i = 0; i < numbers.length; i++) {
-        userChoice[userChoice.length] = numbers[i];
+    if (hasNums) {
+        for (let i = 0; i < numbers.length; i++) {
+            userChoice[userChoice.length] = numbers[i];
+        }
     }
-}
 
-if (hasLower) {
-    for (let j = 0; j < letters.length; j++) {
-        // userChoice.push(letters[j].slice());
-        userChoice[userChoice.length] = letters[j];
+    if (hasLower) {
+        for (let j = 0; j < letters.length; j++) {
+            // userChoice.push(letters[j].slice());
+            userChoice[userChoice.length] = letters[j];
+        }
     }
-}
 
-if (hasUpper) {
-    for (let k = 0; k < letters.length; k++) {
-        // userChoice.push((letters[k].slice()).toUpperCase());
-        userChoice[userChoice.length] = letters[k].toUpperCase();
+    if (hasUpper) {
+        for (let k = 0; k < letters.length; k++) {
+            // userChoice.push((letters[k].slice()).toUpperCase());
+            userChoice[userChoice.length] = letters[k].toUpperCase();
 
-    }   
-}
-
-if (hasChars) {
-    for (let l = 0; l < specials.length; l++) {
-        // userChoice.push(specials[l].slice());
-        userChoice[userChoice.length] = specials[l];
-
+        }
     }
+
+    if (hasChars) {
+        for (let l = 0; l < specials.length; l++) {
+            // userChoice.push(specials[l].slice());
+            userChoice[userChoice.length] = specials[l];
+
+        }
+    }
+    console.log(userChoice);
+    return userChoice
 }
+console.log(howLong);
 console.log(userChoice);
-return userChoice
+
+for (let index = 0; index < howLong; index++) {
+    password[password.length] = userChoice[Math.floor(Math.random() * userChoice.length)];
+    
 }
+password = password.join("");
+alert(`Your password is ${password}`);
+
+return password;
